8c34be9e57b1965177fc9eb7b532e41af8fc5ca3
Tomislav Zivkovic <tomislav.zivkovic@ericsson.com>
2020-10-16 00:55:57 +0200

    add get, put, post, delete

--- src/main/java/summer/health/application/health_app/controllers/PatientController.java
+++ src/main/java/summer/health/application/health_app/controllers/PatientController.java
@@ -1,9 +1,9 @@
 package summer.health.application.health_app.controllers;
 
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import summer.health.application.health_app.model.Patient;
 import summer.health.application.health_app.services.PatientService;
 
 @RequestMapping("/api")
@@ -16,9 +16,40 @@ public class PatientController {
         this.patientService = patientService;
     }
 
+    //GET
     @RequestMapping({"patient"})
     public String listPatients(Model model){
         model.addAttribute("patients", patientService.findAll());
         return "patients/index";
     }
+
+    //POST
+    @PostMapping("newpatient")
+    Patient newPatient(@RequestBody Patient newPatient) {
+        return patientService.save(newPatient);
+    }
+
+    //DELETE
+    @DeleteMapping("/patients/{id}")
+    void deletePatient(@PathVariable Long id) {
+        patientService.deleteById(id);
+    }
+
+    //PUT
+    @PutMapping("/employees/{id}")
+    Patient replaceEmployee(@RequestBody Patient newPatient, @PathVariable Long id) {
+
+        return patientService.findById(id)
+                .map(patient -> {
+                    patient.setName(newPatient.getName());
+                    patient.setLastName(newPatient.getLastName());
+                    return patientService.save(patient);
+                })
+                .orElseGet(() -> {
+                    newPatient.setId(id);
+                    return patientService.save(newPatient);
+                });
+    }
+
+
 }
--- src/main/java/summer/health/application/health_app/model/Patient.java
+++ src/main/java/summer/health/application/health_app/model/Patient.java
@@ -66,6 +66,10 @@ public class Patient {
         return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
